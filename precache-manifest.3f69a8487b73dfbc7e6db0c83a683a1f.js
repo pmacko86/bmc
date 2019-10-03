48c300b46f71f25f4584513afa3e27d84e70c3c7
Peter Macko <pmacko@gmail.com>
2019-10-02 21:39:55 -0400

    Updates

---
+++ precache-manifest.3f69a8487b73dfbc7e6db0c83a683a1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a42176bc2d7ca5c8872791b19ce866",
+    "url": "/bmc/index.html"
+  },
+  {
+    "revision": "3bbca7079cae5f3f07cd",
+    "url": "/bmc/static/js/2.201dc1dc.chunk.js"
+  },
+  {
+    "revision": "03770b7c2266ff4565fb",
+    "url": "/bmc/static/js/main.8355033f.chunk.js"
+  },
+  {
+    "revision": "90771bf725b85888df3a",
+    "url": "/bmc/static/js/runtime-main.46597212.js"
+  }
+]);
