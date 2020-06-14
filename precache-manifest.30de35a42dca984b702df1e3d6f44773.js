fd4754c742becbe6049c0e1ee0f738fc906a5421
Peter Macko <pmacko@gmail.com>
2020-06-13 23:38:56 -0400

    Updates

---
+++ precache-manifest.30de35a42dca984b702df1e3d6f44773.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2261407858c74f17e991b52f37cb5e95",
+    "url": "/bmc/index.html"
+  },
+  {
+    "revision": "196870360de5beba8419",
+    "url": "/bmc/static/js/2.e4cab52e.chunk.js"
+  },
+  {
+    "revision": "81d7652f51886393a920",
+    "url": "/bmc/static/js/main.b057289e.chunk.js"
+  },
+  {
+    "revision": "90771bf725b85888df3a",
+    "url": "/bmc/static/js/runtime-main.46597212.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bmc/precache-manifest.14f278c6d39b835c160dfa87e71c5c31.js"
+  "/bmc/precache-manifest.30de35a42dca984b702df1e3d6f44773.js"
 );
 
 self.addEventListener('message', (event) => {
